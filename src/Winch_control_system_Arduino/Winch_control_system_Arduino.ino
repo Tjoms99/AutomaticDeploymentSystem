f79909d151436015cb343c5c085243f0e1f8758c
Marcus Alexander Tjomsaas <marcus.alex@live.no>
2023-06-23 23:52:35 +0200

    Added esp32-c3 mqtt

---
+++ src/Winch_control_system_Arduino/Winch_control_system_Arduino.ino
@@ -0,0 +1,78 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// WiFi
+const char *ssid = "Tjoms";         // Enter your Wi-Fi name
+const char *password = "grovt123";  // Enter Wi-Fi password
+
+// MQTT Broker
+const char *mqtt_broker = "test.mosquitto.org";
+
+const char *topic_system = "AutomaticDeploymentSystem/app/system";
+const char *topic_sampling = "AutomaticDeploymentSystem/app/sampling";
+const char *topic_temperature = "AutomaticDeploymentSystem/USS/temperature";
+const char *topic_pressure = "AutomaticDeploymentSystem/USS/pressure";
+const char *topic_depth = "AutomaticDeploymentSystem/USS/depth";
+
+const char *mqtt_username = "";
+const char *mqtt_password = "";
+const int mqtt_port = 1883;
+
+char isSampling[1] = {'f'};
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  // Set software serial baud to 115200;
+  Serial.begin(115200);
+  // Connecting to a WiFi network
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the Wi-Fi network");
+  //connecting to a mqtt broker
+  client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
+  while (!client.connected()) {
+    String client_id = "esp32-client-";
+    client_id += String(WiFi.macAddress());
+    Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
+    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+      Serial.println("Public EMQX MQTT broker connected");
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+  //  subscribe
+  client.subscribe(topic_sampling);
+}
+
+void callback(char *topic, byte *payload, unsigned int length) {
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  isSampling[0] = (char)payload[0];
+  Serial.println();
+  Serial.println("-----------------------");
+
+}
+
+void loop() {
+  static int temperature = 0;
+
+  client.loop();
+  delay(1000);
+  if (isSampling[0] == 't') {
+    client.publish(topic_temperature, "20");
+    client.publish(topic_pressure, "101000");
+    client.publish(topic_depth, "-12");
+  }
+}
