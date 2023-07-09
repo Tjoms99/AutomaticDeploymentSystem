5936488fc52e7904c8361b6e61aeadc4198156fe
Marcus Alexander Tjomsaas <marcus.alex@live.no>
2023-07-09 17:06:42 +0200

    Removed build from DRS

--- src/Drum_relay_system/build/image_preload.cmake
+++
@@ -1,8 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf9160dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
-set(BOARD_ROOT "f:/automaticdeploymentsystem/src/drum_relay_system;C:/ncs/v2.4.0/nrf;C:/ncs/v2.4.0/zephyr" CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE "F:/AutomaticDeploymentSystem/src/Drum_relay_system/prj.conf" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:\ncs\toolchains\31f4403e35\opt\bin\python.exe" CACHE INTERNAL "NCS child image controlled")
