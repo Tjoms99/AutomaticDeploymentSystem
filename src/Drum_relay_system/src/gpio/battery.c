3c4893d93d9a82164187620fcff15751078a4925
Marcus Alexander Tjomsaas <marcus.alex@live.no>
2023-07-18 21:54:42 +0200

    Refacotred battery reading

--- src/Drum_relay_system/src/gpio/battery.c
+++ src/Drum_relay_system/src/gpio/battery.c
@@ -1,24 +1,23 @@
 #include "battery.h"
 
 #include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/adc.h>
 
 static const struct device *const gpio_dev = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+static const struct device *adc_dev = DEVICE_DT_GET(DT_NODELABEL(adc));
 
 #define GPIO_BATTERY_CHARGE_SPEED 13
 #define GPIO_BATTERY_CHARGING_ENABLE 17
 #define GPIO_BATTERY_READ_ENABLE 14
-/*
-static const struct device *adc_dev = DEVICE_DT_GET(DT_NODELABEL(adc));
+
+static uint8_t is_initialized = false;
 
 #define ADC_RESOLUTION 10
 #define ADC_CHANNEL 7
 #define ADC_PORT SAADC_CH_PSELP_PSELP_AnalogInput0 // AIN7
 #define ADC_REFERENCE ADC_REF_INTERNAL             // 0.6V
-#define ADC_GAIN ADC_GAIN_1_5                      // ADC REFERENCE * 5 // ADC_GAIN_1_6
+#define ADC_GAIN ADC_GAIN_1_6                      // ADC REFERENCE * 5 // ADC_GAIN_1_6 ADC REFERENCE * 6
 
 struct adc_channel_cfg channel_7_cfg = {
     .gain = ADC_GAIN,
@@ -36,8 +35,6 @@ struct adc_sequence sequence = {
     .buffer = sample_buffer,
     .buffer_size = sizeof(sample_buffer),
     .resolution = ADC_RESOLUTION};
-*/
-static uint8_t is_initialized = false;
 
 static int battery_enable_read()
 {
@@ -87,23 +84,23 @@ int battery_get_voltage()
 {
     int ret = 0;
 
-    // battery_charge_stop();
-    /*
-        ret |= adc_read(adc_dev, &sequence);
-        if (ret)
-        {
-            printk("ADC read failed (error %d)", ret);
-        }
+    ret |= adc_read(adc_dev, &sequence);
+    if (ret)
+    {
+        printk("ADC read failed (error %d)", ret);
+    }
 
-        int32_t mv_value = sample_buffer[0];
-        printk("ADC value: %d \n", mv_value);
+    int32_t mv_value = sample_buffer[0];
+    printk("ADC value: %d \n", mv_value);
 
-        int32_t adc_vref = adc_ref_internal(adc_dev);
-        ret |= adc_raw_to_millivolts(adc_vref, ADC_GAIN, ADC_RESOLUTION, &mv_value);
-        printk("ADC volt: %d mv\n", mv_value);
+    int32_t adc_vref = adc_ref_internal(adc_dev);
+    ret |= adc_raw_to_millivolts(adc_vref, ADC_GAIN, ADC_RESOLUTION, &mv_value);
+    printk("ADC volt: %d mv\n", mv_value);
 
-        return mv_value;
-        */
+    // int battery_volt = mv_value *rc = val * (uint64_t)dcp->full_ohm / dcp->output_ohm;
+
+    return mv_value;
+
     return 0;
 }
 
@@ -121,7 +118,7 @@ int battery_init()
     if (!device_is_ready(gpio_dev))
     {
         printk("GPIO device not found!");
-        return -EIO;
+        // return -EIO;
     }
 
     ret |= gpio_pin_configure(gpio_dev, GPIO_BATTERY_CHARGING_ENABLE, GPIO_OUTPUT);
@@ -132,24 +129,21 @@ int battery_init()
     if (ret)
     {
         printk("GPIO configure failed!");
-        return ret;
+        // return ret;
     }
 
-    /*
-        // ADC
-        if (!device_is_ready(adc_dev))
-        {
-            printk("ADC device not found!");
-            return -EIO;
-        }
+    if (!device_is_ready(adc_dev))
+    {
+        printk("ADC device not found!");
+        // return -EIO;
+    }
 
-        ret |= adc_channel_setup(adc_dev, &channel_7_cfg);
+    ret |= adc_channel_setup(adc_dev, &channel_7_cfg);
 
-        if (ret)
-        {
-            printk("ADC setup failed (error %d)", ret);
-        }
-    */
+    if (ret)
+    {
+        printk("ADC setup failed (error %d)", ret);
+    }
     is_initialized = true;
 
     return ret;
--- src/Drum_relay_system/src/main.c
+++ src/Drum_relay_system/src/main.c
@@ -16,31 +16,6 @@
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS 1000
 
-static const struct device *adc_dev = DEVICE_DT_GET(DT_NODELABEL(adc));
-
-#define ADC_RESOLUTION 10
-#define ADC_CHANNEL 7
-#define ADC_PORT SAADC_CH_PSELP_PSELP_AnalogInput0 // AIN7
-#define ADC_REFERENCE ADC_REF_INTERNAL			   // 0.6V
-#define ADC_GAIN ADC_GAIN_1_5					   // ADC REFERENCE * 5 // ADC_GAIN_1_6
-
-struct adc_channel_cfg channel_7_cfg = {
-	.gain = ADC_GAIN,
-	.reference = ADC_REFERENCE,
-	.acquisition_time = ADC_ACQ_TIME_DEFAULT,
-	.channel_id = ADC_CHANNEL,
-#ifdef CONFIG_ADC_NRFX_SAADC
-	.input_positive = ADC_PORT
-#endif
-};
-
-int16_t sample_buffer[1];
-struct adc_sequence sequence = {
-	.channels = BIT(ADC_CHANNEL),
-	.buffer = sample_buffer,
-	.buffer_size = sizeof(sample_buffer),
-	.resolution = ADC_RESOLUTION};
-
 int main(void)
 {
 	int ret = 0;
@@ -54,36 +29,11 @@ int main(void)
 	battery_init();
 	battery_charge_start();
 	// Led green on successful initialization.
-	// ADC
-	if (!device_is_ready(adc_dev))
-	{
-		printk("ADC device not found!");
-		return -EIO;
-	}
-
-	ret |= adc_channel_setup(adc_dev, &channel_7_cfg);
-
-	if (ret)
-	{
-		printk("ADC setup failed (error %d)", ret);
-	}
 
 	while (1)
 	{
 		k_msleep(SLEEP_TIME_MS);
-
-		ret |= adc_read(adc_dev, &sequence);
-		if (ret)
-		{
-			printk("ADC read failed (error %d)", ret);
-		}
-
-		int32_t mv_value = sample_buffer[0];
-		printk("ADC value: %d \n", mv_value);
-
-		int32_t adc_vref = adc_ref_internal(adc_dev);
-		ret |= adc_raw_to_millivolts(adc_vref, ADC_GAIN, ADC_RESOLUTION, &mv_value);
-		printk("ADC volt: %d mv\n", mv_value);
+		battery_get_voltage();
 		// rs485_write("4");
 
 		// uart_print_depth();
